c9952412dd3a1e27c3591da86bcd0e7404c56ce7
Paul Strzalkowski <paulwind123@gmail.com>
2024-10-02 15:45:22 +0200

    Update MenuManager.cs
    variables to store

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,8 @@ using UnityEngine;
 public class MenuManager : MonoBehaviour
 {
     public static MenuManager Instance;
+    public String highscoreUsername;
+    public int highscore;
 
     private void Awake()
     {
@@ -15,5 +18,24 @@ public class MenuManager : MonoBehaviour
         }
         Instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void SetHihgscoreUsername(String hsUsername)
+    {
+        MenuManager.Instance.highscoreUsername = hsUsername;
+    }
+
+    public void SetHighscore(int hs)
+    {
+        MenuManager.Instance.highscore = hs;
+    }
+
+    private void Start()
+    {
+        if (MenuManager.Instance != null)
+        {
+            SetHihgscoreUsername(MenuManager.Instance.highscoreUsername);
+            SetHighscore(MenuManager.Instance.highscore);
+        }
     }
 }
