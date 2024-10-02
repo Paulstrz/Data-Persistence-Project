94a6e0f7308f48948723c25508132c87284a43fe
Paul Strzalkowski <paulwind123@gmail.com>
2024-10-02 15:47:22 +0200

    add MenuUIHandler

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DefaultExecutionOrder(1000)]
+public class MenuUIHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
